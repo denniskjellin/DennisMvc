43daf67f93d92700d5d4d8a40a9700e7019ab31a
Dennis Kjellin <denniskjellin@hotmail.com>
2023-02-07 11:29:45 +0100

    adding some settings for input validation

--- Models/Destinations.cs
+++ Models/Destinations.cs
@@ -10,9 +10,16 @@ namespace DennisMvc.Models
         public int Id { get; set; }
 
         [Required]
+        [Display(Name = "Destination name:")]
+        [MaxLength(25)]
+        [MinLength(1)]
+        [RegularExpression("[a-zA-Z]+", ErrorMessage = "Only a-z are allowed in 'Destination name' input.")]
         public String? DestinationName { get; set; }
 
         [Required]
+        [MinLength(1)]
+        [Url]
+        [Display(Name = "Destination Url:")]
         public String? DestinationUrl { get; set; }
     }
 }
