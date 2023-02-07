620bc702330b95d0e1bbb2e29bf381019c616ee7
Dennis Kjellin <denniskjellin@hotmail.com>
2023-02-07 13:49:05 +0100

    fixed so spaces can be entered

--- Models/Destinations.cs
+++ Models/Destinations.cs
@@ -13,7 +13,7 @@ namespace DennisMvc.Models
         [Display(Name = "Destination name:")]
         [MaxLength(25)]
         [MinLength(1)]
-        [RegularExpression("[a-zA-Z]+", ErrorMessage = "Only a-z are allowed in 'Destination name' input.")]
+        [RegularExpression("[a-zA-Z ]+", ErrorMessage = "Only a-z are allowed in 'Destination name' input.")]
         public String? DestinationName { get; set; }
 
         [Required]
